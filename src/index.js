5168821a266aad5593eea1a6fb3d4746a9f7d032
Alexander Neupokoev <admin@armavir.ru>
2020-09-21 21:21:53 +0300

    init: task morse decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,28 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let letters = expr.match(/.{1,10}/g)
+    let resultString =''
+    let result = ''
+    letters.forEach(letter => {
+        let morse = letter.match(/.{1,2}/g)
+        morse.forEach(item => {
+            if (item === '10') result +='.'
+            if (item === '11') result +='-'
+            if (item === '**') result +=' '
+        })
+        if (result === '     ') {
+            resultString += ' '
+            result = ''
+        }
+        else {
+            resultString += MORSE_TABLE[result]
+            result = ''
+        }
+
+    })
+return  resultString
+
 }
 
 module.exports = {
